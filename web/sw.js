c850d342156fe1f0cc7b67f31d20593b815b1100
a24109445 <a24109445@gmail.com>
2026-06-10 23:27:49 +0800

    Force PWA refresh for ZXing scanner

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tomicacollector-web-v2';
+const CACHE_NAME = 'tomicacollector-web-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -29,12 +29,12 @@ self.addEventListener('fetch', (event) => {
   }
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) {
-        return cached;
-      }
-
-      return fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
